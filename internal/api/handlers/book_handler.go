f8a8163489f7299fdc0bf054da908b684a529c55
Susheel Sathyaraj <susheel.sathyaraj@gmail.com>
2025-10-23 22:01:37 +0530

    removing the json decoder and headerwriter for get apis

--- internal/api/handlers/book_handler.go
+++ internal/api/handlers/book_handler.go
@@ -43,13 +43,6 @@ func (h *BookHandler) CreateBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *BookHandler) GetAllBooks(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-
-	//decode json
-	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
 	//service layer
 	booksList, err := h.service.GetAllBooks()
 	if err != nil {
@@ -58,22 +51,16 @@ func (h *BookHandler) GetAllBooks(w http.ResponseWriter, r *http.Request) {
 
 	//set response headers
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
 
 	//send response
 	json.NewEncoder(w).Encode(booksList)
 }
 
 func (h *BookHandler) GetBookByID(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
 	//extract id from path
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	//decode json
-	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
 	//service layer
 	bookDetails, err := h.service.GetBookByID(id)
 	if err != nil {
@@ -82,7 +69,6 @@ func (h *BookHandler) GetBookByID(w http.ResponseWriter, r *http.Request) {
 	}
 	//set response headers
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
 
 	//send response
 	json.NewEncoder(w).Encode(bookDetails)
